100c7d6f4754edf5027352e4bab11b52f58ae4a4
hainv1 <hainv1@huce.edu.vn>
2026-02-06 13:43:08 +0700

    Hoàn thành test Etabs API

--- DALTUDTXD_TenDeTai_MSSV_68TH12/Views/UserControls/UC_Ribbon.xaml.cs
+++ DALTUDTXD_TenDeTai_MSSV_68TH12/Views/UserControls/UC_Ribbon.xaml.cs
@@ -33,5 +33,11 @@ namespace DALTUDTXD_TenDeTai_MSSV_68TH12.Views.UserControls
         {
             Mainframe.Content = new Plan2DPage();
         }
+
+        private void rbt_KetNoiEtabs_Click(object sender, RoutedEventArgs e)
+        {
+            KetNoiEtabsView ketNoiEtabsView = new KetNoiEtabsView();
+            ketNoiEtabsView.ShowDialog();
+        }
     }
 }
